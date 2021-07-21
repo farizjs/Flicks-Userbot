cca9adab1b8bcb2d7a7472f5efe56b0db716c2fb
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-21 18:18:53 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"♨️KEN-UBOT♨️ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"♨️𝐊𝐄𝐍-𝐔𝐁𝐎𝐓♨️ ⚙️ V{BOT_VER} [HAS BEEN ACTIVATED!]")
 
 
 if len(argv) not in (1, 3, 4):
