851e3a273206bdf59295926ccad9792f83d43c45
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-21 18:52:58 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -53,7 +53,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
         if HEROKU_APP_NAME is None:
             await event.edit(
                 '`[HEROKU]: Harap Siapkan Variabel` **HEROKU_APP_NAME** `'
-                ' untuk dapat deploy perubahan terbaru dari ♨️KEN-UBOT♨️.`'
+                ' untuk dapat deploy perubahan terbaru dari ♨️𝐊𝐄𝐍-𝐔𝐁𝐎𝐓♨️`'
             )
             repo.__del__()
             return
@@ -67,7 +67,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
             )
             return repo.__del__()
         await event.edit('`[HEROKU]:'
-                         '\nSedang MengUpdate ♨️KEN-UBOT♨️, Mohon Menunggu 5-7 Menit`'
+                         '\nSedang MengUpdate 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Mohon Menunggu 5-7 Menit`'
                          )
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
@@ -115,9 +115,9 @@ async def update(event, repo, ups_rem, ac_br):
     except GitCommandError:
         repo.git.reset("--hard", "FETCH_HEAD")
     await update_requirements()
-    await event.edit('**♨️KEN-UBOT♨️** `Berhasil Di Update!`')
+    await event.edit('**𝐊𝐄𝐍-𝐔𝐁𝐎𝐓** `Berhasil Di Update!`')
     await asyncio.sleep(1)
-    await event.edit('**♨️KEN-UBOT♨️** `Di Restart....`')
+    await event.edit('**𝐊𝐄𝐍-𝐔𝐁𝐎𝐓** `Di Restart....`')
     await asyncio.sleep(1)
     await event.edit('`Mohon Menunggu Beberapa Detik.`')
     await asyncio.sleep(10)
@@ -126,7 +126,7 @@ async def update(event, repo, ups_rem, ac_br):
     if BOTLOG:
         await event.client.send_message(
             BOTLOG_CHATID, "#BOT \n"
-            "**♨️KEN-UBOT♨️ Telah Di Perbarui.**")
+            "**𝐊𝐄𝐍-𝐔𝐁𝐎𝐓 Telah Di Perbarui.**")
         await asyncio.sleep(100)
         await event.delete()
 
@@ -188,13 +188,13 @@ async def upstream(event):
 
     if changelog == '' and force_update is False:
         await event.edit(
-            f'\n♨️KEN-UBOT♨️ Sudah Versi Terbaru\n')
+            f'\n➣ 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓 Sudah Versi Terbaru\n')
         await asyncio.sleep(15)
         await event.delete()
         return repo.__del__()
 
     if conf is None and force_update is False:
-        changelog_str = f'**Pembaruan Untuk ♨️KEN-UBOT♨️ :\n\n Pembaruan Data :**\n`{changelog}`'
+        changelog_str = f'**Pembaruan Untuk 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓 :\n\n➣ Pembaruan Data :**\n`{changelog}`'
         if len(changelog_str) > 4096:
             await event.edit("`Changelog Terlalu Besar, Lihat File Untuk Melihatnya.`")
             file = open("output.txt", "w+")
@@ -208,18 +208,18 @@ async def upstream(event):
             remove("output.txt")
         else:
             await event.edit(changelog_str)
-        return await event.respond('**Perintah Untuk Update, Sebagai Berikut.**\n 𝘾𝙤𝙢𝙢𝙖𝙣𝙙: >`.update now`\n 𝘾𝙤𝙢𝙢𝙖𝙣𝙙: >`.update deploy`\n\n__Untuk Meng Update Fitur ♨️KEN-UBOT♨️.__')
+        return await event.respond('**Perintah Untuk Update, Sebagai Berikut.**\n 𝘾𝙤𝙢𝙢𝙖𝙣𝙙: >`.update now`\n 𝘾𝙤𝙢𝙢𝙖𝙣𝙙: >`.update deploy`\n\n__Untuk Meng Update Fitur ♨️𝐊𝐄𝐍-𝐔𝐁𝐎𝐓♨️.__')
 
     if force_update:
         await event.edit(
             '`Sinkronisasi Paksa Ke Kode Userbot Stabil Terbaru, Harap Tunggu .....`')
     else:
-        await event.edit('` Proses Update ♨️KEN-UBOT♨️, Loading....1%`')
-        await event.edit('` Proses Update ♨️KEN-UBOT♨️, Loading....20%`')
-        await event.edit('` Proses Update ♨️KEN-UBOT♨️, Loading....35%`')
-        await event.edit('` Proses Update ♨️KEN-UBOT♨️, Loading....77%`')
-        await event.edit('` Proses Update ♨️KEN-UBOT♨️, Updating...90%`')
-        await event.edit('` Proses Update ♨️KEN-UBOT♨️, Mohon Tunggu Sebentar....100%`')
+        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Loading....1%`')
+        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Loading....20%`')
+        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Loading....35%`')
+        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Loading....77%`')
+        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Updating...90%`')
+        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Mohon Tunggu Sebentar....100%`')
     if conf == "now":
         await update(event, repo, ups_rem, ac_br)
         await asyncio.sleep(10)
