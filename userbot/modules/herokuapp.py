2c24ef327a6a2470e5f4c329dccff1520b7e358a
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-21 15:45:05 +0700

    Update herokuapp.py

--- userbot/modules/herokuapp.py
+++ userbot/modules/herokuapp.py
@@ -181,7 +181,7 @@ async def dyno_usage(dyno):
             await dyno.edit(
                 " **Dyno Usage :**  \n"
                 "   \n"
-                "➣`Penggunaan Dyno {ALIVE_NAME} :` \n"
+                "➣`Penggunaan Dyno {APP_NAME} :` \n"
                 f"   • {AppHours} jam - {AppMinutes} menit"
                 f" [{AppPercentage}%] \n"
                 "   \n"
