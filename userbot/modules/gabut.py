6674174701feb529ba94822f2ffa76c17db7ac41
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-07-23 09:33:59 +0700

    fixes: gabut

--- userbot/modules/gabut.py
+++ userbot/modules/gabut.py
@@ -15,7 +15,7 @@ async def typewriter(typew):
     await typew.edit(f"**JAKA SEMBUNG BAWA GOLOK**")
     sleep(3)
     await typew.edit("**NIMBRUNG GOBLOKK!!!**")
-# Owner @Si_Dian
+# Pantun
 
 
 @register(outgoing=True, pattern='^.p(?: |$)(.*)')
@@ -25,7 +25,7 @@ async def typewriter(typew):
     await typew.edit(f"**Halo gaesss..**")
     sleep(1)
     await typew.edit("**السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ**")
-# Owner @manusiarakitann
+# Salam 
 
 
 @register(outgoing=True, pattern='^.l(?: |$)(.*)')
@@ -35,28 +35,43 @@ async def typewriter(typew):
     await typew.edit(f"**Jawab Salam Dulu Gaes**")
     sleep(2)
     await typew.edit("**وَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ**")
-# Owner @manusiarakitann
+# Menjawab Salam
 
 
-@register(outgoing=True, pattern='^Usage(?: |$)(.*)')
+@register(outgoing=True, pattern='^.usangen(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
     await typew.edit("`Getting Information...`")
     sleep(1)
-    await typew.edit("**Dyno Usage**:\n\n╭━━━━━━━━━━━━━━━━━━━━╮\n" f"-> `Penggunaan Dyno ` **{ALIVE_NAME}**:\n" f" •**0 jam - " f"0 menit - 0%**" "\n ◐━─━─━─━─━──━─━─━─━─━◐\n" "-> `Sisa Dyno Bulan Ini`:\n" f" •**9999 jam - 9999 menit " f"- 100%**\n" "╰━━━━━━━━━━━━━━━━━━━━╯"
+    await typew.edit("**Dyno Usage**:\n\n╭━━━━━━━━━━━━━━━━━━━━╮\n" f"-> `Penggunaan Dyno ` **{DEFAULTUSER}**:\n" f" •**0 jam - " f"0 menit - 0%**" "\n ◐━─━─━─━─━──━─━─━─━─━◐\n" "-> `Sisa Dyno Bulan Ini`:\n" f" •**9999 jam - 9999 menit " f"- 100%**\n" "╰━━━━━━━━━━━━━━━━━━━━╯"
                      )
-# @mixiologist
+# Dyno fake
+
+@register(outgoing=True, pattern="^.perkenalan(?: |$)(.*)")
+async def perkenalan(event):
+    event.pattern_match.group(1)
+    await event.edit(f"**Hai Guys , Perkenalkan Nama Gw {DEFAULTUSER}**")
+    sleep(2)
+    await event.edit(f"**Gw Tinggal Di {WEATHER_DEFICITY}**")
+    sleep(2)
+    await event.edit("`Salam Kenal...`")
+    sleep(2)
+    await event.edit("`Udah Gitu Aja :v`")
+# Perkenalan
 
 
-CMD_HELP.update({
-    "fakedyno":
-    "`Usage`\
-\nUsage: tipu tipu anjeeeng.\
-\n\n`.l`\
-\nUsage: Untuk Menjawab Salam.\
-\n\n`.g`\
-\nUsage: Member Goblok.\
-\n\n`.p`\
-\nUsage: Untuk Memberi Salam."
-})
+CMD_HELP.update(
+    {
+        "gabut": "**Modules** - `Gabut`\
+        \n\n 𝘾𝙤𝙢𝙢𝙖𝙣𝙙 : `.usangen`\
+        \nUsage : Dyno fake\
+        \n\n 𝘾𝙤𝙢𝙢𝙖𝙣𝙙 : `.l`\
+        \nUsage : Untuk Menjawab Salam\
+        \n\n 𝘾𝙤𝙢𝙢𝙖𝙣𝙙 : `.g`\
+        \nUsage : Member Goblok\
+        \n\n 𝘾𝙤𝙢𝙢𝙖𝙣𝙙 : `.p`\
+        \nUsage : Untuk Memberi Salam\
+    "
+    }
+)
