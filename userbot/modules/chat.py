73e7ef692f706d83542aef073cfcd6e1b6068b5c
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-29 01:54:06 +0700

    Update chat.py

--- userbot/modules/chat.py
+++ userbot/modules/chat.py
@@ -108,7 +108,7 @@ async def log(log_text):
 @register(outgoing=True, pattern="^.kickme$")
 async def kickme(leave):
     """ Basically it's .kickme command """
-    await leave.edit(f"`Tuanmu Telah Meninggalkan Group Ini Untuk Selamanya...`")
+    await leave.edit(f"`{ALIVE_NAME} Telah Meninggalkan Group....`")
     await leave.client.kick_participant(leave.chat_id, 'me')
 
 
