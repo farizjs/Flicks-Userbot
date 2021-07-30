ac4587287bcd396c84e398ca0b1c034702499eaf
Kennedy <abdulmutaqin14@gmail.com>
2021-07-30 11:12:21 +0700

    ...

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"♨️𝐊𝐄𝐍-𝐔𝐁𝐎𝐓♨️ ⚙️ V{BOT_VER} [HAS BEEN ACTIVATED!]")
+    f"𝐊𝐄𝐍-𝐔𝐁𝐎𝐓 🔧 V{BOT_VER} [Has Been Activated!]")
 
 
 if len(argv) not in (1, 3, 4):
