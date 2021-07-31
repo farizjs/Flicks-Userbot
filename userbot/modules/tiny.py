c52198f04b17bf167ca231925dfd2e7047098649
Kennedy <abdulmutaqin14@gmail.com>
2021-07-31 10:58:05 +0700

    ...

--- userbot/modules/tiny.py
+++ userbot/modules/tiny.py
@@ -14,19 +14,19 @@ async def _(event):
     if not (reply and (reply.media)):
         await event.edit("`Mohon Balas Ke Sticker`")
         return
-    xx = await event.edit("`Memproses Tiny....`")
+    xx = await event.edit("`Bismillah Jadi Kecil...`")
     ik = await bot.download_media(reply)
     im1 = Image.open("resources/extras/ken.png")
     if ik.endswith(".tgs"):
-        await event.client.download_media(reply, "geez.tgs")
-        os.system("lottie_convert.py geez.tgs json.json")
+        await event.client.download_media(reply, "ken.tgs")
+        os.system("lottie_convert.py ken.tgs json.json")
         json = open("json.json", "r")
         jsn = json.read()
         json.close()
         jsn = jsn.replace("512", "2000")
         open("json.json", "w").write(jsn)
-        os.system("lottie_convert.py json.json geez.tgs")
-        file = "geez.tgs"
+        os.system("lottie_convert.py json.json ken.tgs")
+        file = "ken.tgs"
         os.remove("json.json")
     elif ik.endswith((".gif", ".mp4")):
         iik = cv2.VideoCapture(ik)
