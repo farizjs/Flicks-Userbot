ef44dcf2672455e0363583db03b889f32c53a00b
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-31 17:44:20 +0000

    pylint: auto fixes
    Signed-off-by: KennedyProject <abdulmutaqin14@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -382,7 +382,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} 」".format("「", x), 
+        custom.Button.inline("{} {} 」".format("「", x),
                              data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
