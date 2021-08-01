18feeb6e182d84830f722f4884cc61b8f09b2d76
Kennedy <abdulmutaqin14@gmail.com>
2021-08-02 06:34:19 +0700

    mengonsol:v

--- userbot/modules/memify.py
+++ userbot/modules/memify.py
@@ -58,7 +58,7 @@ async def draw_meme_text(image_path, text):
     os.remove(image_path)
     i_width, i_height = img.size
     m_font = ImageFont.truetype(
-        "userbot/utils/styles/kfonts.otf", int(
+        "userbot/utils/styles/KenFont.otf", int(
             (70 / 640) * i_width)
     )
     if ";" in text:
