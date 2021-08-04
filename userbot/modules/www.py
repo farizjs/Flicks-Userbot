4a0ee4e585d8699799aa8a11a25a0364707ca278
Kennedy <abdulmutaqin14@gmail.com>
2021-08-04 11:44:35 +0700

    asu

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -69,12 +69,12 @@ async def redis(pong):
     """ For .ping command, ping the userbot from any chat.  """
     await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    await pong.edit("`0% ▒▒▒▒▒▒▒▒▒▒`")
-    await pong.edit("`20% ██▒▒▒▒▒▒▒▒`")
-    await pong.edit("`40% ████▒▒▒▒▒▒`")
-    await pong.edit("`60% ██████▒▒▒▒`")
-    await pong.edit("`80% ████████▒▒`")
-    await pong.edit("`100% ██████████`")
+    await pong.edit("**0% ▒▒▒▒▒▒▒▒▒▒**")
+    await pong.edit("**20% ██▒▒▒▒▒▒▒▒**")
+    await pong.edit("**40% ████▒▒▒▒▒▒**")
+    await pong.edit("**60% ██████▒▒▒▒**")
+    await pong.edit("**80% ████████▒▒**")
+    await pong.edit("**100% ██████████**")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await pong.edit(f"**╭━━━━━━━━━━━━━━━━━╮** \n"
@@ -242,7 +242,7 @@ async def pingme(pong):
 
 
 CMD_HELP.update({
-    "ping": "Cmd: `.ping` | `.lping` | `.xping` | `.sping` | `.pings` | `.fping`\
+    "ping": "Cmd: `.ping` | `.lping` | `.xping` | `.sping` | `.fping`\
          \n↳ : Untuk Menunjukkan Ping Bot Anda.\
          \n\nCmd: `.speed`\
          \n↳ : Untuk Menunjukkan Kecepatan Jaringan Anda.\
