ef228078ac43a806b75a12ea57c01154b33af0e4
Kennedy <abdulmutaqin14@gmail.com>
2021-09-03 13:39:43 +0700

    Update salam2.py

--- userbot/modules/salam2.py
+++ userbot/modules/salam2.py
@@ -37,56 +37,24 @@ async def typewriter(typew):
     await typew.edit("**ASTAGFIRULLAHALAZDIM....**")
 
 
-@register(outgoing=True, pattern='^X(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**GRUP SAMPAH KEK GINI MENDING BUBARIN AJA NGENTOT.**")
-
-
 @register(outgoing=True, pattern='^.so(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**SOK KERAS BANGET SI JAMET INI BHAAAKSSSS.**")
 
 
-@register(outgoing=True, pattern='^O(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**JANGAN MAIN BOT MULU ALAY LU GOBLOK NORAK BANGET ANJING CUIHHHHH!!!**")
-
-
-@register(outgoing=True, pattern='^D(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**HEH LU BABU, HARUS SOPAN SAMA MAJIKAN LU GOBLOK. KERJA YANG BENER BIAR MAK LU KAGAK NGELONTE LAGI BUAT KASIH MAKAN LU TOLOL!!!**")
-
-
-@register(outgoing=True, pattern='^F(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**BHAAAAAAAAAKKKKKSSSSS**")
-
-
 CMD_HELP.update({
     "salam2":
-    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `F`\
-\n↳ : Tersedak Meledek.\
-\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `X`\
-\n↳ : Ngatain Grup.\
-\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.en`\
-\n↳ : Coba Aja Sendiri Kontol.\
-\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ast`\
+    "Cmd: `.en`\
+\n↳ : Coba aja.\
+\n\nCmd: `.ast`\
 \n↳ : Istigfar.\
-\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.gjm`\
+\n\nCmd: `.gjm`\
 \n↳ : Coba Aja Sendiri.\
-\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `eg`\
+\n\nCmd: `eg`\
 \n↳ : Coba Aja.\
-\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.yhh`\
-\n↳ : Si Wahyu.\
-\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.so`\
-\n↳ : Ngatain Orang Sok Keras.\
-\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `D`\
-\n↳ : Cara Jadi Majikan Yang Benar.\
-\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `O`\
-\n↳ : Ngatain Yang Main Bot Mulu."
+\n\nCmd: `.yhh`\
+\n↳ : Coba aja.\
+\n\nCmd: `.so`\
+\n↳ : Si sokap."
 })
