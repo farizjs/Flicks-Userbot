ab4f8eca41f15f022f559f91d3f8660358616cfb
Kennedy <87056837+KennedyProject@users.noreply.github.com>
2021-09-22 00:49:41 +0700

    well

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -17,4 +17,18 @@ from .format import parse_pre
 from .chrome import chrome, options
 from .google_images_download import googleimagesdownload
 from .progress import progress, CancelProcess
-from .tools import humanbytes, time_formatter, human_to_bytes, run_cmd, md5, post_to_telegraph, take_screen_shot, runcmd
+from .tools import (
+    humanbytes,
+    time_formatter,
+    human_to_bytes,
+    md5,
+    check_media,
+    run_cmd,
+    runcmd,
+    take_screen_shot,
+    post_to_telegraph,
+    media_to_pic,
+    edit_delete,
+    edit_or_reply,
+    media_type
+)
