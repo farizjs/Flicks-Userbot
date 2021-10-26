b86126213159ba375c125ae09e9f7fe13832efc4
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-10-27 00:26:17 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -19,7 +19,7 @@ from PIL import Image
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 REPOLINK = str(
-    UPSTREAM_REPO_URL) if UPSTREAM_REPO_URL else "https://github.com/vckyou/Geez-UserBot"
+    UPSTREAM_REPO_URL) if UPSTREAM_REPO_URL else "https://github.com/fjgaming212/Flicks-Userbot"
 # ============================================
 
 opener = urllib.request.build_opener()
@@ -59,7 +59,7 @@ async def sleepybot(time):
 @register(outgoing=True, pattern="^.shutdown$")
 async def killdabot(event):
     """ For .shutdown command, shut the bot down."""
-    await event.edit("`Mematikan KEN-UBOT....`")
+    await event.edit("`Mematikan Flick-Userbot....`")
     await asyncio.sleep(7)
     await event.delete()
     if BOTLOG:
@@ -70,7 +70,7 @@ async def killdabot(event):
 
 @register(outgoing=True, pattern="^.restart$")
 async def killdabot(event):
-    await event.edit("`Restarting KEN-UBOT...`")
+    await event.edit("`Restarting Flicks-Userbot...`")
     await asyncio.sleep(10)
     await event.delete()
     if BOTLOG:
@@ -87,7 +87,7 @@ async def killdabot(event):
 async def reedme(e):
     await e.edit(
         "Here's Something for You to Read :\n"
-        "\n[KEN-UBOT](https://github.com/KennedyProject/KEN-UBOT/blob/KEN-UBOT/README.md)"
+        "\n[Flicks-Userbot](https://github.com/fjgaming212/Flicks-Userbot/blob/Flicks-Userbot/README.md)"
         "\n[Setup Guide - Basic](https://telegra.ph/How-to-host-a-Telegram-Userbot-11-02)"
         "\n[Special - Note](https://telegra.ph/Special-Note-11-02)")
 
@@ -110,9 +110,9 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "**Hey**, I am using **𝐊𝐄𝐍-𝐔𝐁𝐎𝐓** \n"
-        "➣ **Repo :** [Github](https://github.com/KennedyProject/KEN-UBOT)\n"
-        "➣ **Owner :** [Ken](t.me/xgothboi)"
+        "**Hey**, I am using **Flicks-Userbot** \n"
+        "➣ **Repo :** [Github](https://github.com/fjgaming212/Flicks-Userbot)\n"
+        "➣ **Owner :** [Skyzo](t.me/xflskyzo)"
     )
 
 
