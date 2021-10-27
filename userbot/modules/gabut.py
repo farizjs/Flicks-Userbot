69e8db2cff3c1efaddca7ed7565e170bfb1e6fc0
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-10-28 05:28:13 +0700

    Update gabut.py

--- userbot/modules/gabut.py
+++ userbot/modules/gabut.py
@@ -35,6 +35,28 @@ async def typewriter(typew):
 # Menjawab Salam
 
 
+@register(outgoing=True, pattern=r"^\.virtual(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**OOOO**")
+    sleep(1.5)
+    await typew.edit("**INI YANG VIRTUAL**")
+    sleep(1.5)
+    await typew.edit("**YANG KATANYA SAYANG BANGET**")
+    sleep(1.5)
+    await typew.edit("**TAPI TETEP AJA DI TINGGAL**")
+    sleep(1.5)
+    await typew.edit("**NI INGET**")
+    sleep(1.5)
+    await typew.edit("**TANGANNYA AJA GA BISA DI PEGANG**")
+    sleep(1.5)
+    await typew.edit("**APALAGI OMONGANNYA**")
+    sleep(1.5)
+    await typew.edit("**BHAHAHAHA**")
+    sleep(1.5)
+    await typew.edit("**KASIAN MANA MASIH MUDA**")
+
+
 @register(outgoing=True, pattern="^.alay(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
