544d439f99e0f27fee088a3a9f1786dd965a8428
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2021-10-28 19:20:12 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -33,7 +33,7 @@ async def help(event):
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`\t|  "
-        await event.edit("**✨Flicks-Userbot✨**\n\n"
+        await event.edit("**✨𝐅𝐥𝐢𝐜𝐤𝐬-𝐔𝐬𝐞𝐫𝐛𝐨𝐭​✨**\n\n"
                          f"**❒ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**❒ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
                          "**❒ Mᴀɪɴ Mᴇɴᴜ :**\n"
                          f"◉| {string}◉\n\n"
