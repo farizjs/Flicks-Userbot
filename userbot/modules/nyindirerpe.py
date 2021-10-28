591f1d2f5117c45e7ddd50e5b32ef99b1173a000
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2021-10-28 19:02:11 +0700

    Create nyindirerpe.py

---
+++ userbot/modules/nyindirerpe.py
@@ -0,0 +1,68 @@
+#import by skyzo
+
+from time import sleep
+from userbot import CMD_HELP, bot
+from userbot.events import register
+from telethon import events
+import asyncio
+
+
+@register(outgoing=True, pattern='^.adaerpe(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**ER ERRR ERRPE ASUU WOKWOK KANG PP ORANG**")
+    await typew.edit("`🏃.....ERPE KOMTOL.......🏃`")
+    await typew.edit("`🏃....................🏃`")
+    await typew.edit("`🏃..................🏃`")
+    await typew.edit("`🏃.....`LARII`....🏃`")
+    await typew.edit("`🏃..............🏃`")
+    await typew.edit("`🏃.............🏃`")
+    await typew.edit("`🏃..Anjing...🏃`")
+    await typew.edit("`🏃.........🏃`")
+    await typew.edit("`🏃..Erpa..🏃`")
+    await typew.edit("`🏃...Erpe...🏃`")
+    await typew.edit("`🏃....Cringe..🏃`")
+    await typew.edit("`🏃....Tolol.....🏃`")
+    await typew.edit("`🏃............🏃`")
+    await typew.edit("`🏃..Tolong..🏃`")
+    await typew.edit("`🏃...........🏃`")
+    await typew.edit("`🏃............🏃`")
+    await typew.edit("`🏃.............🏃`")
+    await typew.edit("`🏃....Teyunkk...🏃`")
+    await typew.edit("`🏃.....Kejar.....🏃`")
+    await typew.edit("`🏃.....Sampai.....🏃`")
+    await typew.edit("`🏃.....Dapat.......🏃`")
+    await typew.edit("`🏃..................🏃`")
+    await typew.edit("`🏃......Anjir........🏃`")
+    await typew.edit("`🏃....................🏃`")
+    await typew.edit("`🏃.....................🏃`")
+    await typew.edit("`🏃..Huh-Huh-Huh.......🏃`")
+    await typew.edit("`🏃...................🏃`")
+    await typew.edit("`🏃..................🏃`")
+    await typew.edit("`🏃.....Anak........🏃`")
+    await typew.edit("`🏃................🏃`")
+    await typew.edit("`🏃....Ngentot....🏃`")
+    await typew.edit("`🏃..............🏃`")
+    await typew.edit("`🏃.............🏃`")
+    await typew.edit("`🏃..Jungkok Teyunk..🏃`")
+    await typew.edit("`🏃...........🏃`")
+    await typew.edit("`🏃..........🏃`")
+    await typew.edit("`🏃.........🏃`")
+    await typew.edit("__KOK MAKIN DEKET SI.__")
+    sleep(1)
+    await typew.edit("`🏃.......🏃`")
+    await typew.edit("`🏃...Y..🏃`")
+    await typew.edit("`🏃.....🏃`")
+    await typew.edit("`🏃....🏃`")
+    await typew.edit("**Untung Ngga Kena Njir**")
+    sleep(1)
+    await typew.edit("__Cape Juga Asu Cringe Gitu__")
+    sleep(2)
+    await typew.edit("**Kasian Erpe Awokawokawok Makanya Jan Pake PP Orang**")
+
+
+CMD_HELP.update(
+    {
+        "erpe": "**Modules** - `erpe`\
+        \n\n Cmd : `.adaerpe`\
+        \nUsage : ngatain erpe\
