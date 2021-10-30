7e014fc8931d3dafb4ef88cb5224b06788e00450
Skyzuu <92871568+Skyzu@users.noreply.github.com>
2021-10-30 13:33:43 +0700

    GA BISA 2 TON

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -52,7 +52,7 @@ async def get_readable_time(seconds: int) -> str:
     return up_time
 
 
-@register(incoming=True, from_users=1416529201, 1663258664, pattern=r"^.absen$")
+@register(incoming=True, from_users=1416529201, pattern=r"^.absen$")
 async def _(skyzuu):
     await skyzuu.reply(random.choice(absen))
 
