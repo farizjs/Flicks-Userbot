c92a3f3d506d270a6d054b488a90ee0fca385f9e
FJ_GAMING <84852749+fjgaming212@users.noreply.github.com>
2021-11-14 14:08:10 +0700

    Update offline.py

--- userbot/modules/offline.py
+++ userbot/modules/offline.py
@@ -270,7 +270,7 @@ CMD_HELP.update({
 \nUsage: Sama Seperti AFK, Lakukan ketika ingin OFF.\nSiapapun Yang Balas, Tag, Atau Chat Kamu \
 Mereka Akan Tau Alasan Kamu OFF.\n\nOFF Bisa Dilakukan Dan Dibatalkan Dimanapun, dan menguubah nama belakang menjadi `✘ ᴏꜰꜰʟɪɴᴇ ✘`.**\
 \n\n**Perintah : `.sibuk`\
-\nUsage : Memberi tahu kalau Anda sedang SIBUK , dan menguubah nama belakang menjadi `✘ sɪʙᴜᴋ ✘`.\
+\nUsage : Memberi tahu kalau Anda sedang SIBUK , dan menguubah nama belakang menjadi `✘ sɪʙᴜᴋ ✘`.**\
 \n\n**Notes : Bila ada orang spam berlebihan ke Anda , tinggal ketik `.block`**\
 "
 })
