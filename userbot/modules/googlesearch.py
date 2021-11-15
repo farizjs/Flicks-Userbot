713e6471570b1ddfc0ed387322212dce8ca62c35
FJ_GAMING <84852749+fjgaming212@users.noreply.github.com>
2021-11-15 20:35:12 +0700

    Create googlesearch.py

---
+++ userbot/modules/googlesearch.py
@@ -0,0 +1,44 @@
+#  © @FJ_GAMING
+# ⚠️ Do not remove credits
+import asyncio
+
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^\.search(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    aing = await event.client.get_me()
+    text = event.pattern_match.group(1)
+    if not text:
+        await event.edit("`Give a teks too!`")
+    else:
+        await event.edit("`Processing...`")
+    chat = "@tdtapibot"
+    async with event.client.conversation(chat) as conv:
+        try:
+            msg = await conv.send_message(f"{text}")
+            response = await conv.get_response()
+            logo = await conv.get_response()
+            """ - don't spam notif - """
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.edit(
+                "**Error: Mohon Buka Blokir** @ribot **Dan Coba Lagi!**"
+            )
+            return
+        await asyncio.sleep(0.5)
+        await event.client.send_message(
+            event.chat_id,
+            message,
+        )
+        await event.client.delete_messages(conv.chat_id, [msg.id, response.id])
+        await event.delete()
+
+
+CMD_HELP.update({"logo": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.search` <teks>"
+                 "\n↳ : Untuk mencari sesuatu di google."})
