8070aa421ed533adee9d42d13a178d45d9733058
FJ_GAMING <84852749+fjgaming212@users.noreply.github.com>
2021-11-19 14:43:45 +0700

    Update flickshelper.py

--- userbot/modules/flickshelper.py
+++ userbot/modules/flickshelper.py
@@ -14,7 +14,7 @@ async def usit(e):
         f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
         "\n[Owner](t.me/FJ_GAMING)"
         "\n[Repo](https://github.com/fjgaming212/Flicks-Userbot)"
-        "\nTeam [Skyzo](t.me/xflskyzo) [Kyy](t.me/IDnyaKosong) [Tonic](t.me/bukan_guudlooking)")
+        "\nTeam [Klick Here](https://t.me/devoloperflicks/32)")
 
 
 @register(outgoing=True, pattern="^.vars$")
