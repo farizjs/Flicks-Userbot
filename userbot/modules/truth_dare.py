0da7ccfa2664dd74cbe7213544955c6974898506
FJ_GAMING <84852749+fjgaming212@users.noreply.github.com>
2021-11-22 14:16:55 +0700

    Update truth_dare.py

--- userbot/modules/truth_dare.py
+++ userbot/modules/truth_dare.py
@@ -9,7 +9,7 @@ from userbot.events import register
 @register(outgoing=True, pattern=r"^\.truth(?: |$)(.*)")
 async def _(event):
     await event.edit("Mengirim pesan truth...")
-    async with bot.conversation("@truthordaresbot") as conv:
+    async with bot.conversation("@truthordares_bot") as conv:
         try:
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=1206492140)
@@ -18,7 +18,7 @@ async def _(event):
             response = await response
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await event.edit("Harap unblock `@truthordaresbot` dan coba lagi")
+            await event.edit("Harap unblock `@truthordares_bot` dan coba lagi")
             return
         await event.edit(f"**Pesan truth**\n\n{response.message.message}")
 
@@ -26,7 +26,7 @@ async def _(event):
 @register(outgoing=True, pattern=r"^\.dare(?: |$)(.*)")
 async def _(event):
     await event.edit("Mengirim pesan dare...")
-    async with bot.conversation("@truthordaresbot") as conv:
+    async with bot.conversation("@truthordares_bot") as conv:
         try:
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=1206492140)
@@ -35,7 +35,7 @@ async def _(event):
             response = await response
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await event.edit("Harap unblock `@truthordaresbot` dan coba lagi")
+            await event.edit("Harap unblock `@truthordares_bot` dan coba lagi")
             return
         await event.edit(f"**Pesan dare**\n\n{response.message.message}")
 
