6c01c6b9863b374e0f2106643ecb3bc02010281a
fjgaming212 <ff.fjindo@gmail.com>
2021-11-26 12:10:37 +0000

    pylint: auto fixes
    Signed-off-by: fjgaming212 <ff.fjindo@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -589,7 +589,8 @@ with bot:
             builder = event.builder
             result = None
             query = event.text
-            if event.query.user_id == uid and query.startswith("@FlicksSupport"):
+            if event.query.user_id == uid and query.startswith(
+                    "@FlicksSupport"):
                 buttons = paginate_help(0, dugmeler, "helpme")
                 result = builder.photo(
                     file=kenlogo,
