7c7aeec97c78f7292b93eec761d7edc3a41c6651
FJ_GAMING <84852749+fjgaming212@users.noreply.github.com>
2021-11-26 15:00:06 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -357,7 +357,7 @@ with bot:
 
 
 async def check_alive():
-    await bot.send_file(BOTLOG_CHATID, ALIVE_LOGO, caption=f"**Flicks Userbot Telah diaktifkan ✨**\n\n✥ Master : {ALIVE_NAME}\n✥ Username : @{c.username}\n✥ Plugin : {len(plugins)}\n✥ Botver : {BOT_VER}\n✥ Support: @FlicksSupport")
+    await bot.send_file(BOTLOG_CHATID, ALIVE_LOGO, caption=f"**Flicks Userbot Telah diaktifkan ✨**\n\n✥ Master : {ALIVE_NAME}\n✥ Plugin : {len(plugins)}\n✥ Botver : {BOT_VER}\n✥ Support: @FlicksSupport")
     return
 
 with bot:
