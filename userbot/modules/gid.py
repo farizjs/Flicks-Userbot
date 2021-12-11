75ea60a94a22b92681c34731c1c77e5ad26c4c66
Fariz <84852749+fjgaming212@users.noreply.github.com>
2021-12-11 17:52:32 +0700

    Update gid.py

--- userbot/modules/gid.py
+++ userbot/modules/gid.py
@@ -39,7 +39,7 @@ async def image_maker(event):
 
 CMD_HELP.update(
     {
-        "gid": "⚡**CMD**⚡ : `.gid`\
-        \n↳ : Reply to a user to generate ID Card."
+        "gid": "**Perintah** : `.gid`\
+        \n↳ : Balas ke pengguna untuk menghasilkan ID."
     }
 )
