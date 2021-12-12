ae0db3cca0ab6dae0e59a35636c6fc06e3ff6528
Fariz <84852749+fjgaming212@users.noreply.github.com>
2021-12-12 18:58:07 +0700

    Update tempmail.py

--- userbot/modules/tempmail.py
+++ userbot/modules/tempmail.py
@@ -1,3 +1,5 @@
+# From Geez-Userbot
+
 from telethon import events
 from userbot import CMD_HELP, bot
 from userbot.events import register
@@ -8,7 +10,7 @@ import asyncio
 @register(outgoing=True, pattern=r"^\.tempmail(?: |$)(.*)")
 async def _(event):
     chat = "@TempMailBot"
-    geez = await event.edit("Sedang Memprosess...")
+    flicks = await event.edit("Sedang Memprosess...")
     async with bot.conversation(chat) as conv:
         try:
             response = conv.wait_event(events.NewMessage(
@@ -20,12 +22,12 @@ async def _(event):
             await asyncio.sleep(1)
             await conv.send_message("Generate New")
             response = await response
-            geezuserbot = ((response).reply_markup.rows[2].buttons[0].url)
+            flicksuserbot = ((response).reply_markup.rows[2].buttons[0].url)
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await geez.edit("`Mohon Maaf, Silahkan Buka` @TempMailBot `Lalu Tekan Start dan Coba Lagi.`")
+            await flicks.edit("`Mohon Maaf, Silahkan Buka` @TempMailBot `Lalu Tekan Start dan Coba Lagi.`")
             return
-        await event.edit(f"**FLICKS TEMPMAIL** ~ `{response.message.message}`\n\n[KLIK DISINI UNTUK VERIFIKASI]({geezuserbot})")
+        await event.edit(f"**FLICKS TEMPMAIL** ~ `{response.message.message}`\n\n[KLIK DISINI UNTUK VERIFIKASI]({flicksuserbot})")
 
 
 CMD_HELP.update({"tempmail": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.tempmail`"
