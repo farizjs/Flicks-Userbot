fd1874f4574827e634ada40abe20ba396da48f36
Fariz <84852749+fjgaming212@users.noreply.github.com>
2021-12-13 17:53:42 +0700

    Update inlineon.py

--- userbot/modules/inlineon.py
+++ userbot/modules/inlineon.py
@@ -8,21 +8,19 @@ from userbot.events import register
 @register(outgoing=True, pattern=r"^\.inlineon(?: |$)(.*)")
 async def _(event):
     await event.edit(f"Sedang menyalakan inline untuk @{BOT_USERNAME} tunggu sebentar")
-            async with bot.conversation("@BotFather") as conv:
-                try:
+    async with bot.conversation("@BotFather") as conv:
+        try:
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=93372553)
             )
-                    await conv.send_message("/setinline")
-                    await conv.get_response()
-                    await conv.send_message(f"@{BOT_USERNAME}")
-                    await conv.get_response()
-                    await conv.send_message("Search")
-                    await conv.get_response()
-                    await bot.send_read_acknowledge(conv.chat_id)
-                except YouBlockedUserError:
-                    return await event.edit("Unblock @Botfather first.")
-                await event.edit(
-                    "**Berhasil Menyalakan Mode Inline**\n\n**Ketik** `.helpme` **lagi untuk membuka menu bantuan.**"
-                )
-            )
+            await conv.send_message("/setinline")
+            await conv.get_response()
+            await conv.send_message(f"@{BOT_USERNAME}")
+            await conv.get_response()
+            await conv.send_message("Search")
+            await conv.get_response()
+            await bot.send_read_acknowledge(conv.chat_id)
+         except YouBlockedUserError:
+            await event.edit("Unblock @Botfather first.")
+            return
+                await event.edit("**Berhasil Menyalakan Mode Inline**\n\n**Ketik** `.helpme` **lagi untuk membuka menu bantuan.**")
