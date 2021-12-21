1e62ff6d67213f94763371977eb8ccbb562bc5f9
Fariz <84852749+fjgaming212@users.noreply.github.com>
2021-12-21 18:26:50 +0700

    Create tutorialflicks.py

---
+++ userbot/modules/tutorialflicks.py
@@ -0,0 +1,31 @@
+# © Fariz ☘️
+# Kalo mau di ambil ambil aja,tapi kalo commandnya jadi aneh jangan
+# salahkan saya🚶
+import logging
+
+from userbot import BOT_USERNAME
+from userbot.events import register
+
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    level=logging.WARNING)
+
+
+@register(outgoing=True, pattern=r"^\.tutorial")
+async def yardim(event):
+    try:
+        kenbotusername = BOT_USERNAME
+        if kenbotusername is not None:
+            results = await event.client.inline_query(kenbotusername, "tutor")
+            await results[0].click(
+                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+            )
+            await event.delete()
+        else:
+            await event.edit(
+                "`Botnya tidak berfungsi! Silahkan atur vars `BOT_TOKEN` dan `BOT_USERNAME` dengan benar.\ntau gunakan perintah `.set var BOT_TOKEN` <token> dan `.set var BOT_USERNAME` <Username Bot mu>."
+            )
+    except Exception:
+        return await event.edit(
+            "**Anda tidak dapat mengirim inline menu dalam obrolan ini, silakan gunakan perintah** `.inlineon`"
+        )
