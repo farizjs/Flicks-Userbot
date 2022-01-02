fca163ec266193556df3e6146635236e15316989
fjgaming212 <ff.fjindo@gmail.com>
2022-01-02 02:35:54 +0000

    pylint: auto fixes
    Signed-off-by: fjgaming212 <ff.fjindo@gmail.com>

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -19,7 +19,6 @@ import time
 from datetime import datetime
 import psutil
 from userbot import ALIVE_LOGO, ALIVE_NAME, BOT_VER, CMD_HELP, FLICKS_TEKS_KUSTOM, StartTime, UPSTREAM_REPO_BRANCH, bot
-from userbot.events import register
 from userbot.utils import flicks_cmd
 from userbot import CMD_HANDLER as cmd
 
