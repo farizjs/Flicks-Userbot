04037cbc90154264333b3b5da908fc2e04229a26
fjgaming212 <ff.fjindo@gmail.com>
2022-01-03 05:59:57 +0000

    pylint: auto fixes
    Signed-off-by: fjgaming212 <ff.fjindo@gmail.com>

--- userbot/modules/flicksdetek.py
+++ userbot/modules/flicksdetek.py
@@ -1,7 +1,5 @@
-from userbot.events import register
 from userbot import CMD_HELP, bot
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import CMD_HANDLER as cmd
 from userbot.utils import flicks_cmd
 
 
