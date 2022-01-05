e5d0ea0e12c498c9870ba3810a3e5d6968f1dfe7
Fariz <84852749+fjgaming212@users.noreply.github.com>
2022-01-05 12:29:30 +0700

    Update youtube_downloader.py

--- userbot/modules/youtube_downloader.py
+++ userbot/modules/youtube_downloader.py
@@ -6,9 +6,11 @@ from youtube_dl import YoutubeDL
 
 from userbot.events import register
 from userbot import CMD_HELP
+from userbot import CMD_HANDLER as cmd
+from userbot.utils import flicks_cmd
 
 
-@register(outgoing=True, pattern=".yt(a|v|sa|sv) (.*)", disable_errors=True)
+@flicks_cmd(pattern="yt(a|v|sa|sv) (.*)"
 async def download_from_youtube_(event):
     opt = event.pattern_match.group(1).lower()
     if opt == "a":
@@ -94,12 +96,12 @@ async def download_from_youtube_(event):
 
 CMD_HELP.update({
     "ytdownload":
-    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.yta` <(youtube) link>\
-   \nUsage : Download audio from the link.\
-   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ytv <(youtube) link>`\
-   \nUsage : Download video  from the link.\
-   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ytsa <(youtube) search query>`\
-   \nUsage : Search and download audio from youtube.\
-   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ytsv <(youtube) search query>`\
-   \nUsage : Search and download video from youtube."
+    f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}yta` <(youtube) link>\
+   \nUsage : Unduh audio dari tautan.\
+   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}ytv <(youtube) link>`\
+   \nUsage : Unduh video dari tautan.\
+   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}ytsa <(youtube) kueri penelusuran>`\
+   \nUsage : Cari dan unduh audio dari youtube.\
+   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}ytsv <(youtube) search query>`\
+   \nUsage : Cari dan unduh video dari youtube."
 })
