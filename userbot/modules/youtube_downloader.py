2269c5d4d625be436c278b9ff73a40a87c7e99c3
Fariz <84852749+fjgaming212@users.noreply.github.com>
2022-01-05 12:30:52 +0700

    fix

--- userbot/modules/youtube_downloader.py
+++ userbot/modules/youtube_downloader.py
@@ -10,11 +10,11 @@ from userbot import CMD_HANDLER as cmd
 from userbot.utils import flicks_cmd
 
 
-@flicks_cmd(pattern="yt(a|v|sa|sv) (.*)"
+@flicks_cmd(pattern="yt(a|v|sa|sv) (.*)")
 async def download_from_youtube_(event):
-    opt=event.pattern_match.group(1).lower()
+    opt = event.pattern_match.group(1).lower()
     if opt == "a":
-        ytd=YoutubeDL(
+        ytd = YoutubeDL(
             {
                 "format": "bestaudio",
                 "writethumbnail": True,
@@ -24,16 +24,16 @@ async def download_from_youtube_(event):
                 "outtmpl": "%(id)s.mp3",
             }
         )
-        url=event.pattern_match.group(2).lower()
+        url = event.pattern_match.group(2).lower()
         if not url:
             return await event.edit("Give me a (youtube) URL to download audio from!")
         try:
             request.get(url)
         except BaseException:
             return await event.edit("`Give A Direct Audio Link To Download`")
-        xx=await event.edit(get_string("com_1"))
+        xx = await event.edit(get_string("com_1"))
     elif opt == "v":
-        ytd=YoutubeDL(
+        ytd = YoutubeDL(
             {
                 "format": "best",
                 "writethumbnail": True,
@@ -43,16 +43,16 @@ async def download_from_youtube_(event):
                 "outtmpl": "%(id)s.mp4",
             }
         )
-        url=event.pattern_match.group(2).lower()
+        url = event.pattern_match.group(2).lower()
         if not url:
             return await event.edit("Give me a (youtube) URL to download video from!")
         try:
             request.get(url)
         except BaseException:
             return await event.edit("`Give A Direct Video Link To Download`")
-        xx=await event.edit(get_string("com_1"))
+        xx = await event.edit(get_string("com_1"))
     elif opt == "sa":
-        ytd=YoutubeDL(
+        ytd = YoutubeDL(
             {
                 "format": "bestaudio",
                 "writethumbnail": True,
@@ -63,15 +63,15 @@ async def download_from_youtube_(event):
             }
         )
         try:
-            query=event.text.split(" ", 1)[1]
+            query = event.text.split(" ", 1)[1]
         except IndexError:
             return await event.edit("Give me a (youtube) search query to download audio from!"
                                     )
-        xx=await event.edit("`Searching on YouTube...`")
-        url=await get_yt_link(query)
+        xx = await event.edit("`Searching on YouTube...`")
+        url = await get_yt_link(query)
         await xx.edit("`Downloading audio song...`")
     elif opt == "sv":
-        ytd=YoutubeDL(
+        ytd = YoutubeDL(
             {
                 "format": "best",
                 "writethumbnail": True,
@@ -82,12 +82,12 @@ async def download_from_youtube_(event):
             }
         )
         try:
-            query=event.text.split(" ", 1)[1]
+            query = event.text.split(" ", 1)[1]
         except IndexError:
             return await event.edit("Give me a (youtube) search query to download video from!"
                                     )
-        xx=await event.edit("`Searching YouTube...`")
-        url=await get_yt_link(query)
+        xx = await event.edit("`Searching YouTube...`")
+        url = await get_yt_link(query)
         await xx.edit("`Downloading video song...`")
     else:
         return
