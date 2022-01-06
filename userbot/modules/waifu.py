2ef807e73daddee7f4b61fe37ff3afd135072837
Fariz <84852749+fjgaming212@users.noreply.github.com>
2022-01-06 11:11:44 +0700

    Update waifu.py

--- userbot/modules/waifu.py
+++ userbot/modules/waifu.py
@@ -9,6 +9,9 @@ import os
 
 from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
 from userbot.events import register
+from userbot import CMD_HANDLER as cmd
+from userbot.utils import flicks_cmd
+
 
 
 EMOJI_PATTERN = re.compile(
@@ -32,7 +35,7 @@ def deEmojify(inputString: str) -> str:
     return re.sub(EMOJI_PATTERN, '', inputString)
 
 
-@register(outgoing=True, pattern="^.waifu(?: |$)(.*)")
+@flicks_cmd(pattern="waifu(?: |$)(.*)")
 async def waifu(animu):
     #"""Generate random waifu sticker with the text!"""
 
@@ -61,7 +64,7 @@ async def waifu(animu):
     await animu.delete()
 
 
-@register(outgoing=True, pattern=r'^.hz(:? |$)(.*)?')
+@flicks_cmd(pattern="hz(:? |$)(.*)?")
 async def _(hazmat):
     await hazmat.edit("`Sending information...`")
     level = hazmat.pattern_match.group(2)
@@ -132,8 +135,8 @@ async def _(hazmat):
 
 CMD_HELP.update({
     "waifu":
-    "`.waifu` text\
+    f"`{cmd}waifu` text\
 \nUsage: for custom stickers.\
-\n\n`.hz` or `.hz [flip, x2, rotate (degree), background (number), black]`\
+\n\n`{cmd}hz` or `{cmd}hz [flip, x2, rotate (degree), background (number), black]`\
 \nUsage: Reply to a image / sticker to suit up!."
 })
