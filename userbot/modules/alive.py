17fb70c9ae66ffaf125b3c9237fa37fe32d6b5f0
Fariz <84852749+fjgaming212@users.noreply.github.com>
2022-01-11 20:39:14 +0700

    Update alive.py

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -9,7 +9,6 @@
 import asyncio
 from asyncio import create_subprocess_exec as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
-import logging
 from platform import python_version, uname
 from shutil import which
 from os import remove
@@ -22,7 +21,6 @@ import psutil
 from userbot import ALIVE_LOGO, ALIVE_NAME, BOT_VER, CMD_HELP, FLICKS_TEKS_KUSTOM, StartTime, UPSTREAM_REPO_BRANCH, bot
 from userbot.utils import flicks_cmd
 from userbot import CMD_HANDLER as cmd
-from userbot import BOT_USERNAME
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
@@ -334,29 +332,6 @@ async def redis(alive):
         await asyncio.sleep(100)
         await alive.delete()
 
-logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
-    level=logging.WARNING)
-
-
-@flicks_cmd(pattern="xalive")
-async def yardim(event):
-    try:
-        kenbotusername = BOT_USERNAME
-        if kenbotusername is not None:
-            results = await event.client.inline_query(kenbotusername, "flicksalive")
-            await results[0].click(
-                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
-            )
-            await event.delete()
-        else:
-            await event.edit(
-                f"`Botnya tidak berfungsi! Silahkan atur vars `BOT_TOKEN` dan `BOT_USERNAME` dengan benar.\ntau gunakan perintah `{cmd}set var BOT_TOKEN` <token> dan `{cmd}set var BOT_USERNAME` <Username Bot mu>."
-            )
-    except Exception:
-        return await event.edit(
-            f"**Anda tidak dapat mengirim inline menu dalam obrolan ini, silakan gunakan perintah** `{cmd}inlineon`"
-        )
 
 
 @flicks_cmd(pattern="aliveu$")
@@ -390,7 +365,7 @@ CMD_HELP.update({
 })
 CMD_HELP.update({
     "alive":
-    f"Cmd: `{cmd}alive` or `{cmd}flickson` or `{cmd}on` or `{cmd}falive`"
+    f"Cmd: `{cmd}alive` or `{cmd}flickson` or `{cmd}on` or `{cmd}falive` or `{cmd}xalive"
     "\n↳ : To see whether your bot is working or not."
     f"\n\nCmd: `{cmd}aliveu` <text>"
     "\n↳ : Changes the 'user' in alive to the text you want."
