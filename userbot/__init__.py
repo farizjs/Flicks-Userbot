550f0e58ed6e2e1f8dd90ce28053454b64aeff67
Fariz <84852749+fjgaming212@users.noreply.github.com>
2022-01-11 20:34:37 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -649,19 +649,7 @@ with bot:
             elif query.startswith("flicksalive"):
                 result = builder.article(
                     "Flicks-Userbot ",
-                    text=f"""
-        [⁣]({ALIVE_LOGO})**The Flicks Userbot** \n
-        {FLICKS_TEKS_KUSTOM} \n\n
-        ┏━━━━━━━━━━━━━━━━━━━ \n
-        ┣  **Master**   : {ALIVE_NAME} \n
-        ┣  **Telethon** :` Ver {version.__version__} `\n
-        ┣  **Python**   :` Ver {python_version()} `\n
-        ┣  **Branch**   :` {UPSTREAM_REPO_BRANCH} `\n
-        ┣  **Bot Ver**  :` v.{BOT_VER} `\n
-        ┣  **Modules**  :` {len(modules)} Modules `\n
-        ┣  **Support**  : @FlicksSupport \n
-        ┗━━━━━━━━━━━━━━━━━━━
-"""
+                    text=f"[⁣]({ALIVE_LOGO})**The Flicks Userbot** \n{FLICKS_TEKS_KUSTOM} \n\n┏━━━━━━━━━━━━━━━━━━━ \n┣  **Master**   : {ALIVE_NAME} \n┣  **Telethon** :` Ver {version.__version__} `\n┣  **Python**   :` Ver {python_version()} `\n┣  **Branch**   :` {UPSTREAM_REPO_BRANCH} `\n┣  **Bot Ver**  :` v.{BOT_VER} `\n┣  **Modules**  :` {len(modules)} Modules `\n┣  **Support**  : @FlicksSupport \n┗━━━━━━━━━━━━━━━━━━━"
                     buttons=[
                         [custom.Button.url(
                                 "ʀᴇᴘᴏ",
