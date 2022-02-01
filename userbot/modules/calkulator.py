002871bf7f22f1d70d91198df8a3a5a23f7ead07
Fariz <84852749+fjgaming212@users.noreply.github.com>
2022-02-01 19:44:27 +0700

    fix invalid pattern

--- userbot/modules/calkulator.py
+++ userbot/modules/calkulator.py
@@ -11,7 +11,7 @@ from userbot import CMD_HELP, CMD_HANDLER as hai
 from userbot.utils import flicks_cmd, edit_or_reply
 
 
-@flicks_cmd(pattern="calc ([\\s\\S]*")
+@flicks_cmd(pattern="calc(?: |$)(.*)*")
 async def calculator(event):
     "Untuk menyelesaikan persamaan matematika dasar."
     cmd = event.text.split(" ", maxsplit=1)[1]
