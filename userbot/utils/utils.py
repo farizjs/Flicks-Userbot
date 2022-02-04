7244fd4852381cc2f61b66bd44c05ee37458616a
Fariz <84852749+fjgaming212@users.noreply.github.com>
2022-02-04 18:05:41 +0700

    fix

--- userbot/utils/utils.py
+++ userbot/utils/utils.py
@@ -116,7 +116,7 @@ async def autobot():
                 "**Tunggu Sebentar, Sedang MeRestart Heroku untuk Menerapkan Perubahan.**",
             )
             heroku_var["BOT_TOKEN"] = token
-            heroku_var["BOT_USERNAME"] = f"@{username}"
+            heroku_var["BOT_USERNAME"] = f"{username}"
         else:
             LOGS.info(
                 "Silakan Hapus Beberapa Bot Telegram Anda di @Botfather atau Set Var BOT_TOKEN dengan token bot"
@@ -158,7 +158,7 @@ async def autobot():
             "**Tunggu Sebentar, Sedang MeRestart Heroku untuk Menerapkan Perubahan.**",
         )
         heroku_var["BOT_TOKEN"] = token
-        heroku_var["BOT_USERNAME"] = f"@{username}"
+        heroku_var["BOT_USERNAME"] = f"{username}"
     else:
         LOGS.info(
             "Silakan Hapus Beberapa Bot Telegram Anda di @Botfather atau Set Var BOT_TOKEN dengan token bot"
