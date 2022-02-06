b2e4689d4f70082bab6c6a206cbe0f664ece77b1
Fariz <84852749+fjgaming212@users.noreply.github.com>
2022-02-06 08:53:53 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -26,6 +26,7 @@ from telethon.utils import get_display_name
 
 from userbot.modules.sql_helper.users_sql import add_user_to_db
 from userbot.modules.sql_helper.blacklistbot_sql import check_is_black_list
+from userbot.modules.sql_helper.users_sql import get_user_id
 
 # Button from Man-Userbot
 # Thanks Man-Userbot 💙
@@ -686,21 +687,43 @@ Perintah yang tersedia di bot ini :
                     f"**PONG!!**\n `{ms}ms`",
                 )
 
-@ken.tgbot.on(events.NewMessage(func=lambda e: e.is_private))
-async def one_new_mssg(event):
-    incoming = event.raw_text
-    who = event.sender_id
-    if check_is_black_list(who):
-        return
-    if incoming.startswith("/"):
-        pass
-    elif who == uid:
-        return
-    else:
-        await event.get_sender()
-        event.chat_id
-        to = await event.forward_to(uid)
-        add_user_to_db(to.id, who, event.id)
+        @ken.tgbot.on(events.NewMessage(func=lambda e: e.is_private))
+         async def one_new_mssg(event):
+            incoming = event.raw_text
+            who = event.sender_id
+            if check_is_black_list(who):
+            return
+            if incoming.startswith("/"):
+            pass
+            elif who == uid:
+            return
+             else:
+              await event.get_sender()
+              event.chat_id
+              to = await event.forward_to(uid)
+              add_user_to_db(to.id, who, event.id)
+
+
+
+         @ken.tgbot.on(events.NewMessage(func=lambda e: e.is_private))
+          async def on_out_mssg(event):
+          to_send = await event.get_reply_message()
+          if to_send is None:
+         return
+          to_send.id
+          send_mssg = event.raw_text
+          who = event.sender_id
+          user_id, reply_message_id = get_user_id(to_send.id)
+          if who == uid:
+           if send_mssg.startswith("/"):
+            return
+          if event.text is not None and event.media:
+            # if sending media
+            bot_api_file_id = pack_bot_file_id(event.media)
+            await tgbot.send_file(user_id, file=bot_api_file_id, caption=event.text, reply_to=reply_message_id)
+          else:
+               await tgbot.send_message(user_id, send_mssg, reply_to=reply_message_id,)
+
 
 
         @ ken.tgbot.on(
