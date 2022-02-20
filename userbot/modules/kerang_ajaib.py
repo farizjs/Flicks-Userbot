d878400bcaf2ad9cd3146780c3df687e7e723fc4
Fariz <84852749+farizjs@users.noreply.github.com>
2022-02-20 10:20:10 +0700

    try

---
+++ userbot/modules/kerang_ajaib.py
@@ -0,0 +1,24 @@
+import random
+from userbot.utils import asst_cmd
+
+APAKAH_STRING = ["Iya", 
+                 "Tidak", 
+                 "Mungkin", 
+                 "Mungkin Tidak", 
+                 "Bisa jadi", 
+                 "Mungkin Tidak",
+                 "Tidak Mungkin",
+                 "YNTKTS",
+                 "Coba aja",
+                 "Apa iya?",
+                 "Bisa iya bisa tidak"
+                 ]
+
+
+@asst_cmd(pattern=r"^/apakah ?(.*)")
+async def apakah(event):
+    quew = event.pattern_match.group(1)
+    if not quew:
+        await event.reply('Berikan saya pertanyaan!')
+        return
+    await event.reply(random.choice(APAKAH_STRING))
