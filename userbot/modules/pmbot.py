7140f845513b66541d56ebc061b659eda42e7ce5
Fariz <84852749+farizjs@users.noreply.github.com>
2022-02-24 11:28:17 +0700

    Update pmbot.py

--- userbot/modules/pmbot.py
+++ userbot/modules/pmbot.py
@@ -121,45 +121,6 @@ async def unban_user_from_bot(user, reason, reply_to=None):
     return info
 
 
-async def check_bot_started_users(user, event):
-    if user.id == OWNER_ID:
-        return
-    check = get_starter_details(user.id)
-    if check is None:
-        start_date = str(datetime.now().strftime("%B %d, %Y"))
-        notification = f"🔮 **#BOT_START**\n**First Name:** {_format.mentionuser(user.first_name , user.id)} \
-                \n**User ID: **`{user.id}`\
-                \n**Action: **Telah Memulai saya."
-    else:
-        start_date = check.date
-        notification = f"🔮 **#BOT_RESTART**\n**First Name:** {_format.mentionuser(user.first_name , user.id)}\
-                \n**ID: **`{user.id}`\
-                \n**Action: **Telah Me-Restart saya"
-    try:
-        add_starter_to_db(user.id, get_display_name(user), start_date, user.username)
-    except Exception as e:
-        LOGS.error(str(e))
-    if BOTLOG:
-        await event.client.send_message(BOTLOG_CHATID, notification)
-
-
-@asst_cmd(pattern=r"^/help$", from_users=OWNER_ID)
-async def bot_help(event):
-    await event.reply(
-        f"""**Perintah di Bot ini adalah:**\n
-**NOTE: Perintah ini hanya berfungsi di {botusername}**\n
- • **Command : **/uinfo <reply ke pesan>
- • **Function : **Untuk Mencari Info Pengirim Pesan.\n
- • **Command : **/ban <alasan> atau /ban <username/userid> <alasan>
- • **Function : **Untuk Membanned Pengguna dari BOT.(Gunakan alasan saat ban)\n
- • **Command : **/unban <alasan> atau /unban <username/userid>
- • **Function : **Membuka Banned pengguna dari bot, agar bisa mengirim pesan lagi dibot.
- • **NOTE : **Untuk memeriksa daftar pengguna yang dibanned Ketik `.bblist`\n
- • **Command : **/broadcast
- • **Function : **Balas ke pesan untuk diBroadcast ke setiap pengguna yang memulai bot Anda. Untuk mendapatkan daftar pengguna Ketik `.botuser`\n
- • **NOTE : ** Jika pengguna menghentikan/memblokir bot maka dia akan dihapus dari database Anda yaitu dia akan dihapus dari daftar bot_starters
-"""
-    )
 
 
 @asst_cmd(pattern="^/broadcast$", from_users=OWNER_ID)
@@ -305,86 +266,6 @@ async def ban_starters(event):
     await edit_or_reply(event, msg)
 
 
-@asst_cmd(pattern=f"^/start({botusername})?([\\s]+)?$", func=lambda e: e.is_private)
-async def bot_start(event):
-    chat = await event.get_chat()
-    user = await event.client.get_me()
-    if check_is_black_list(chat.id):
-        return
-    reply_to = await reply_id(event)
-    botlogo = "https://telegra.ph/file/fc57a8c8022bbd2623458.jpg"
-    mention = f"[{chat.first_name}](tg://user?id={chat.id})"
-    my_mention = f"[{user.first_name}](tg://user?id={user.id})"
-    first = chat.first_name
-    last = chat.last_name
-    fullname = f"{first} {last}" if last else first
-    username = f"@{chat.username}" if chat.username else mention
-    userid = chat.id
-    my_first = user.first_name
-    my_last = user.last_name
-    my_fullname = f"{my_first} {my_last}" if my_last else my_first
-    my_username = f"@{user.username}" if user.username else my_mention
-    if chat.id != OWNER_ID:
-        customstrmsg = gvarstatus("START_TEXT") or None
-        if customstrmsg is not None:
-            start_msg = customstrmsg.format(
-                mention=mention,
-                first=first,
-                last=last,
-                fullname=fullname,
-                username=username,
-                userid=userid,
-                my_first=my_first,
-                my_last=my_last,
-                my_fullname=my_fullname,
-                my_username=my_username,
-                my_mention=my_mention,
-            )
-        else:
-            start_msg = f"**👋 Hai** {mention}**!**\
-                        \n\n**Saya adalah {my_first}** \
-                        \n**Anda dapat Menghubungi [{OWNER}](tg://user?id={OWNER_ID}) dari sini.**\
-                        \n**Jangan Melakukan Spam Atau anda akan di blokir**\
-                        \n\n**Powered by** [UserBot](https://github.com/fjgaming212/Flicks-Userbot)"
-        buttons = [
-            (
-                Button.url("ɢʀᴏᴜᴘ", f"https://t.me/FlicksSupport"),
-                Button.url(
-                    "ᴄʜᴀɴɴᴇʟ",
-                    f"https://t.me/InfoFlicksUserbot",
-                ),
-            )
-        ]
-    else:
-        start_msg = f"**Halo [{OWNER}](tg://user?id={OWNER_ID})\
-            \nApa ada yang bisa saya Bantu?\
-            \nSilahkan Ketik /help Bila butuh Bantuan**"
-        buttons = [
-            (
-                Button.url("ɢʀᴏᴜᴘ", f"https://t.me/FlicksSupport"),
-                Button.url(
-                    "ᴄʜᴀɴɴᴇʟ",
-                    f"https://t.me/InfoFlicksUserbot",
-                ),
-            )
-        ]
-    try:
-        await event.client.send_message(
-            chat.id,
-            start_msg,
-            link_preview=False,
-            buttons=buttons,
-            reply_to=reply_to,
-        )
-    except Exception as e:
-        if BOTLOG:
-            await event.client.send_message(
-                BOTLOG_CHATID,
-                f"**ERROR:** Saat Pengguna memulai Bot anda.\\\x1f                \n`{e}`",
-            )
-
-    else:
-        await check_bot_started_users(chat, event)
 
 
 @tgbot.on(events.CallbackQuery(data=b"about"))
