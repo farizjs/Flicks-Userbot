8e4b50d37722f20ed84416a3c1127b896073291e
Fariz <84852749+farizjs@users.noreply.github.com>
2022-03-08 17:48:22 +0700

    Flicks-Userbot: Remake Module

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -1,43 +1,89 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot help command """
+#    TeleBot - UserBot
+#    Copyright (C) 2020 TeleBot
 
-import asyncio
-from userbot import ALIVE_NAME, CMD_HELP
-from platform import uname
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#    Recode by Fariz <Github.com/farizjs>
+#    From Flicks-Userbot
+#    <t.me/TheFlicksUserbot>
+
+import os
+
+from userbot import ALIVE_NAME, BOT_USERNAME, CMD_HELP, CMD_HANDLER, CMD_LIST, bot, tgbot
 from userbot.utils import flicks_cmd
-from userbot import CMD_HANDLER as cmd
 
-modules = CMD_HELP
-
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-# ============================================
+user = bot.get_me()
+DEFAULTUSER = user.first_name
+CUSTOM_HELP_EMOJI = "⚡"
 
 
-@flicks_cmd(pattern="help(?: |$)(.*)")
-async def help(event):
-    """ For .help command,"""
-    args = event.pattern_match.group(1).lower()
-    if args:
-        if args in CMD_HELP:
-            await event.edit(str(CMD_HELP[args]))
+
+
+@flicks_cmd(pattern="help ?(.*)")
+async def cmd_list(event):
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        tgbotusername = BOT_USERNAME
+        input_str = event.pattern_match.group(1)
+        if tgbotusername is None or input_str == "text":
+            string = ""
+            for i in CMD_HELP:
+                string += CUSTOM_HELP_EMOJI + " " + i + " " + CUSTOM_HELP_EMOJI + "\n"
+                for iter_list in CMD_HELP[i]:
+                    string += "    `" + str(iter_list) + "`"
+                    string += "\n"
+                string += "\n"
+            if len(string) > 4095:
+                with io.BytesIO(str.encode(string)) as out_file:
+                    out_file.name = "cmd.txt"
+                    await tgbot.send_file(
+                        event.chat_id,
+                        out_file,
+                        force_document=True,
+                        allow_cache=False,
+                        caption="**COMMANDS**",
+                        reply_to=reply_to_id,
+                    )
+                    await event.delete()
+            else:
+                await event.edit(string)
+        elif input_str:
+            if input_str in CMD_LIST:
+                string = "**✘ Commands available in {} ✘** \n\n".format(input_str)
+                if input_str in CMD_HELP:
+                    for i in CMD_HELP[input_str]:
+                        string += i
+                    string += "\n\n**💕 @TheFlicksUserbot**"
+                    await event.edit(string)
+                else:
+                    for i in CMD_LIST[input_str]:
+                        string += "    " + i
+                        string += "\n"
+                    string += "\n**💕 @TheFlicksUserbot**"
+                    await event.edit(string)
+            else:
+                await event.edit(input_str + " **bukan plugin yang valid!**")
         else:
-            await event.edit(f"**Module** `{args}` **Tidak tersedia🚶**")
-            await asyncio.sleep(6)
-            await event.delete()
-    else:
-        string = ""
-        for i in CMD_HELP:
-            string += "`" + str(i)
-            string += "`\t|  "
-        await event.edit("**✨𝐅𝐥𝐢𝐜𝐤𝐬-𝐔𝐬𝐞𝐫𝐛𝐨𝐭​✨**\n\n"
-                         f"**❒ Bᴏᴛ ᴏꜰ {DEFAULTUSER} **\n**❒ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
-                         "**❒ Mᴀɪɴ Mᴇɴᴜ :**\n"
-                         f"◉| {string}◉\n\n"
-                         f"\n**Contoh** : Ketik <`{cmd}help offline`> Untuk Informasi Pengunaan Perintah.\nAtau Bisa Juga Ketik `{cmd}helpme` Untuk Main Menu Yang Lain-Nya.")
-        await asyncio.sleep(360)
-        await event.delete()
+            help_string = f"""Pembantu Userbot untuk {DEFAULTUSER}"""
+            try:
+                results = await bot.inline_query(  # pylint:disable=E0602
+                    tgbotusername, "@FlicksSupport"
+                )
+                await results[0].click(
+                    event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+                )
+                await event.delete()
+            except BaseException:
+                await event.edit(
+                    f"** Sepertinya obrolan atau bot ini tidak mendukung inline mode.**"
+                )
