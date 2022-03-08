dd2ca4004778dfa7adc18c94e73d1a2ffaaaf0ad
Fariz <84852749+farizjs@users.noreply.github.com>
2022-03-08 19:45:33 +0700

    Update _help.py

--- userbot/modules/_help.py
+++ userbot/modules/_help.py
@@ -32,49 +32,12 @@ CUSTOM_HELP_EMOJI = "⚡"
 
 @flicks_cmd(pattern="help ?(.*)")
 async def cmd_list(event):
-    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        tgbotusername = BOT_USERNAME
-        input_str = event.pattern_match.group(1)
-        if tgbotusername is None or input_str == "text":
-            string = ""
-            for i in CMD_HELP:
-                string += CUSTOM_HELP_EMOJI + " " + i + " " + CUSTOM_HELP_EMOJI + "\n"
-                for iter_list in CMD_HELP[i]:
-                    string += "    `" + str(iter_list) + "`"
-                    string += "\n"
-                string += "\n"
-            if len(string) > 4095:
-                with io.BytesIO(str.encode(string)) as out_file:
-                    out_file.name = "cmd.txt"
-                    await tgbot.send_file(
-                        event.chat_id,
-                        out_file,
-                        force_document=True,
-                        allow_cache=False,
-                        caption="**COMMANDS**",
-                        reply_to=reply_to_id,
-                    )
-                    await event.delete()
-            else:
-                await event.edit(string)
-        elif input_str:
-            if input_str in CMD_LIST:
-                string = "**✘ Commands available in {} ✘** \n\n".format(input_str)
-                if input_str in CMD_HELP:
-                    for i in CMD_HELP[input_str]:
-                        string += i
-                    string += "\n\n**💕 @TheFlicksUserbot**"
-                    await event.edit(string)
-                else:
-                    for i in CMD_LIST[input_str]:
-                        string += "    " + i
-                        string += "\n"
-                    string += "\n**💕 @TheFlicksUserbot**"
-                    await event.edit(string)
-            else:
-                await event.edit(input_str + " **bukan plugin yang valid!**")
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await event.edit(f"✘ Commands available in {args} ✘ \n\n" + str(CMD_HELP[args]) + "\n\n💕 @TheFlicksUserbot")
         else:
-            help_string = f"""Pembantu Userbot untuk {DEFAULTUSER}"""
+            await event.edit(f"**Module** `{args}` **Tidak tersedia!**")
             try:
                 results = await bot.inline_query(  # pylint:disable=E0602
                     tgbotusername, "@FlicksSupport"
