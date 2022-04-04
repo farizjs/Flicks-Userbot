850b9aa544b4df1dea35cf5d4fc90ddcc4d40732
Fariz <84852749+farizjs@users.noreply.github.com>
2022-04-04 13:37:28 +0700

    Update calculator.py

--- userbot/modules/calculator.py
+++ userbot/modules/calculator.py
@@ -1,165 +1,58 @@
-# Copyright (C) 2021 Ultroid <https://github.com/Teamultroid/Ultroid>
+# Copyright (C) 2021 Catuserbot <https://github.com/sandy1709/catuserbot>
 # Recode by @farizjs
 # FROM Flicks-Userbot <https://github.com/farizjs/Flicks-Userbot>
 # t.me/TheFlicksUserbot
 
-import re
-from telethon import Button
-from telethon.events import CallbackQuery, InlineQuery, NewMessage
+import io
+import sys
+import traceback
 
-from userbot import *
-from userbot.utils import *
-from userbot.helper import *
-
-user = bot.get_me()
-OWNER_ID = user.id
-CALC = {}
-
-m = [
-    "AC",
-    "C",
-    "⌫",
-    "%",
-    "7",
-    "8",
-    "9",
-    "+",
-    "4",
-    "5",
-    "6",
-    "-",
-    "1",
-    "2",
-    "3",
-    "x",
-    "00",
-    "0",
-    ".",
-    "÷",
-]
-tultd = [Button.inline(f"{x}", data=f"calc{x}") for x in m]
-lst = list(zip(tultd[::4], tultd[1::4], tultd[2::4], tultd[3::4]))
-lst.append([Button.inline("=", data="calc=")])
+from userbot import CMD_HELP, CMD_HANDLER as hai
+from userbot.utils import flicks_cmd, edit_or_reply
 
 
-@flicks_cmd(pattern="calc")
-async def icalc(e):
-    if e.client._bot:
-        return await e.reply("• Flicks Inline Calculator •", buttons=lst)
-    results = await e.client.inline_query(BOT_USERNAME, "calc")
-    await results[0].click(e.chat_id, silent=True, hide_via=True)
-    await e.delete()
-
-
-@in_pattern("calc")
-async def _(e):
-    calc = e.builder.article("Calc", text="• Flicks Inline Calculator •", buttons=lst)
-    await e.answer([calc])
-
-
-@callback(data=re.compile(b"calc(.*)"))
-async def _(e):
-    if e.query.user_id == OWNER_ID:
-        x = (e.data_match.group(1)).decode()
-        user = e.query.user_id
-        get = None
-        if x == "AC":
-            if CALC.get(user):
-                CALC.pop(user)
-            await e.edit(
-                "• Flicks Inline Calculator •",
-                buttons=[Button.inline("Buka Kalkulator Lagi", data="recalc")],
-            )
-        elif x == "C":
-            if CALC.get(user):
-                CALC.pop(user)
-            await e.answer("cleared")
-        elif x == "⌫":
-            if CALC.get(user):
-                get = CALC[user]
-            if get:
-                CALC.update({user: get[:-1]})
-                await e.answer(str(get[:-1]))
-        elif x == "%":
-            if CALC.get(user):
-                get = CALC[user]
-            if get:
-                CALC.update({user: get + "/100"})
-                await e.answer(str(get + "/100"))
-        elif x == "÷":
-            if CALC.get(user):
-                get = CALC[user]
-            if get:
-                CALC.update({user: get + "/"})
-                await e.answer(str(get + "/"))
-        elif x == "x":
-            if CALC.get(user):
-                get = CALC[user]
-            if get:
-                CALC.update({user: get + "*"})
-                await e.answer(str(get + "*"))
-        elif x == "=":
-            if CALC.get(user):
-                get = CALC[user]
-            if get:
-                if get.endswith(("*", ".", "/", "-", "+")):
-                    get = get[:-1]
-                out = eval(get)
-                try:
-                    num = float(out)
-                    await e.answer(f"Answer : {num}", cache_time=0, alert=True)
-                except BaseException:
-                    CALC.pop(user)
-                    await e.answer("Kesalahan", cache_time=0, alert=True)
-            await e.answer("None")
-        else:
-            if CALC.get(user):
-                get = CALC[user]
-            if get:
-                CALC.update({user: get + x})
-                return await e.answer(str(get + x))
-            CALC.update({user: x})
-            await e.answer(str(x))
-
+@flicks_cmd(pattern="calc(?: |$)(.*)*")
+async def calculator(event):
+    "Untuk menyelesaikan persamaan matematika dasar."
+    cmd = event.text.split(" ", maxsplit=1)[1]
+    event = await edit_or_reply(event, "Calculating ...")
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+    san = f"print({cmd})"
+    try:
+        await aexec(san, event)
+    except Exception:
+        exc = traceback.format_exc()
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
     else:
-        reply_pop_up_alert = f"Kamu Tidak diizinkan, ini Userbot Milik {ALIVE_NAME}"
-        await e.answer(reply_pop_up_alert, cache_time=0, alert=True)
+        evaluation = "Maaf saya tidak dapat menemukan hasil untuk persamaan yang diberikan"
+    final_output = "**PERSAMAAN**: `{}` \n\n **SOLUSI**: \n`{}` \n".format(
+        cmd, evaluation
+    )
+    await event.edit(final_output)
 
 
-@callback(data=re.compile(b"recalc"))
-async def _(e):
-    if e.query.user_id == OWNER_ID:
-        m = [
-            "AC",
-            "C",
-            "⌫",
-            "%",
-            "7",
-            "8",
-            "9",
-            "+",
-            "4",
-            "5",
-            "6",
-            "-",
-            "1",
-            "2",
-            "3",
-            "x",
-            "00",
-            "0",
-            ".",
-            "÷",
-        ]
-        tultd = [Button.inline(f"{x}", data=f"calc{x}") for x in m]
-        lst = list(zip(tultd[::4], tultd[1::4], tultd[2::4], tultd[3::4]))
-        lst.append([Button.inline("=", data="calc=")])
-        await e.edit("• Flicks Inline Calculator •", buttons=lst)
+async def aexec(code, event):
+    exec("async def __aexec(event): " +
+         "".join(f"\n {l}" for l in code.split("\n")))
 
-    else:
-        reply_pop_up_alert = f"Kamu Tidak diizinkan, ini Userbot Milik {ALIVE_NAME}"
-        await e.answer(reply_pop_up_alert, cache_time=0, alert=True)
+    return await locals()["__aexec"](event)
 
 
-CMD_HELP.update({"calkulator": f"{CMD_HANDLER}calc\n"
-                 f"usage : Inline Calcuator.\n"})
+CMD_HELP.update({"calculator": f"{hai}calc\n"
+                 f"usage : Menyelesaikan permasalahan matematika dasar.\n"
+                 "Memecahkan persamaan matematika yang diberikan dengan aturan BODMAS.\n"
+                 f"contoh : `{hai}calc 2+9`, `{hai}calc 2*2`, `{hai}calc 7-2`, `{hai}calc 9/3`"})
