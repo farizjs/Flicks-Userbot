02ee3d149051669ddb1660b5d01a79d7e339dd0d
Fariz <84852749+farizjs@users.noreply.github.com>
2022-04-07 11:32:23 +0700

    Update _help.py

--- userbot/modules/_help.py
+++ userbot/modules/_help.py
@@ -1,19 +1,6 @@
 #    TeleBot - UserBot
 #    Copyright (C) 2020 TeleBot
 
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 #    Recode by Fariz <Github.com/farizjs>
 #    From Flicks-Userbot
 #    <t.me/TheFlicksUserbot>
@@ -58,5 +45,5 @@ async def cmd_list(event):
             await event.delete()
         except BaseException:
             await event.edit(
-                f"** Sepertinya obrolan atau bot ini tidak mendukung inline mode.**"
+                f"** Sepertinya obrolan atau bot ini tidak mendukung inline mode.\nUntuk alternatif gunakan perintah\n👉`{CMD_HANDLER}plugins`**"
             )
