57f9752aff5467c5eac10ca65739ecec8e577563
Fariz <84852749+farizjs@users.noreply.github.com>
2022-04-25 20:29:48 +0700

    Update spam.py

--- userbot/modules/spam.py
+++ userbot/modules/spam.py
@@ -2,15 +2,35 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
+"""
+Plugin : spam
 
+Perintah : `{i}cspam` <text>
+Penggunaan : Spam teks huruf demi huruf.
+
+Perintah : `{i}spam` <count> <text>
+Penggunaan : Membanjiri teks dalam obrolan !!
+
+Perintah : `{i}wspam` <text>
+Penggunaan : Spam teks kata demi kata.
+
+Perintah : `{i}picspam` <count> <link to image/gif>
+Penggunaan : Seolah-olah spam teks tidak cukup !!
+
+Perintah : `{i}delayspam` <delay> <count> <text>
+Penggunaan : `{i}delayspam` tetapi dengan penundaan khusus
+
+
+CATATAN : Spam dengan risiko Anda sendiri !!"
+"""
 import asyncio
 from asyncio import sleep
 
-from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
-from userbot.events import register
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, CMD_HANDLER
+from userbot.utils import flicks_cmd
 
 
-@register(outgoing=True, pattern="^.cspam (.*)")
+@flicks_cmd(pattern="cspam (.*)")
 async def tmeme(e):
     cspam = str(e.pattern_match.group(1))
     message = cspam.replace(" ", "")
@@ -23,7 +43,7 @@ async def tmeme(e):
             "TSpam was executed successfully")
 
 
-@register(outgoing=True, pattern="^.wspam (.*)")
+@flicks_cmd(pattern="wspam (.*)")
 async def tmeme(e):
     wspam = str(e.pattern_match.group(1))
     message = wspam.split()
@@ -36,7 +56,7 @@ async def tmeme(e):
             "WSpam was executed successfully")
 
 
-@register(outgoing=True, pattern="^.spam (.*)")
+@flicks_cmd(pattern="spam (.*)")
 async def spammer(e):
     counter = int(e.pattern_match.group(1).split(' ', 1)[0])
     spam_message = str(e.pattern_match.group(1).split(' ', 1)[1])
@@ -47,7 +67,7 @@ async def spammer(e):
                                     "Spam was executed successfully")
 
 
-@register(outgoing=True, pattern="^.picspam")
+@flicks_cmd(pattern="picspam")
 async def tiny_pic_spam(e):
     message = e.text
     text = message.split()
@@ -62,7 +82,7 @@ async def tiny_pic_spam(e):
             "PicSpam was executed successfully")
 
 
-@register(outgoing=True, pattern="^.delayspam (.*)")
+@flicks_cmd(pattern="delayspam (.*)")
 async def spammer(e):
     spamDelay = float(e.pattern_match.group(1).split(' ', 2)[0])
     counter = int(e.pattern_match.group(1).split(' ', 2)[1])
@@ -76,18 +96,4 @@ async def spammer(e):
             BOTLOG_CHATID, "#DelaySPAM\n"
             "DelaySpam was executed successfully")
 
-
-CMD_HELP.update({
-    "spam":
-    "`.cspam` <text>\
-\nUsage: Spam the text letter by letter.\
-\n\n`.spam` <count> <text>\
-\nUsage: Floods text in the chat !!\
-\n\n`.wspam` <text>\
-\nUsage: Spam the text word by word.\
-\n\n`.picspam` <count> <link to image/gif>\
-\nUsage: As if text spam was not enough !!\
-\n\n`.delayspam` <delay> <count> <text>\
-\nUsage: `.bigspam` but with custom delay.\
-\n\n\nNOTE : Spam at your own risk !!"
-})
+CMD_HELP.update({"spam": f"{__doc__.format(i=CMD_HANDLER)}"})
